e4d70b7b6068c65c16cb82566d2d081a662ce61c
emin <magomedemin.islamov@mail.ru>
2021-01-19 17:26:31 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import {useDispatch, useSelector} from "react-redux";
+import {useEffect} from "react";
+import {loadTodos} from "./actions";
+
 
 function App() {
+
+  const todos = useSelector(state => state.todos);
+  const loading = useSelector(state => state.loading)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(loadTodos())
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Список дел:
+      {loading ? 'Идет загрузка' : todos.map(todo => {
+          return(
+              <div>
+                <div className='skyblue'> id {todo.id}:</div>
+                <div className='sectionTitle flex'>
+                  <div className='title skyblue'>title:</div>
+                  {todo.title}
+                </div>
+                <div className='bodySection flex'>
+                  <div className='body skyblue'>
+                    body:
+                  </div>
+                  {todo.body}
+                </div>
+              </div>
+          )
+        }) }
+
     </div>
   );
 }
